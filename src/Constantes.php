853fb407910784c15484b515dc28bbdfbfb57883
Agustin-Fernandez <agustin.fernandez.tdlzal@gmail.com>
2019-08-20 17:33:32 -0300

    Arregladas violaciones S y O

--- src/Constantes.php
+++ src/Constantes.php
@@ -4,10 +4,14 @@ namespace TrabajoTarjeta;
 
 class Constantes {
 
-public $cargasPosibles = [
-[100,100],
-[150,200],
-[250,400]
+public static $cargasPosibles = [
+  [10, 10],
+  [20, 20],
+  [30, 30],
+  [50, 50],
+  [100, 100],
+  [510.15, 592.08],
+  [962.59, 1184.17]
 ];
 
 }

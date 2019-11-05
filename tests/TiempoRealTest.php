db1c66ff45ca3f4d4c230d9eb17f8815c1dc55c4
Agustin-Fernandez <agustin.fernandez.tdlzal@gmail.com>
2019-11-05 10:55:32 -0300

    Removido __Construct en TiempoRealTest

--- tests/TiempoRealTest.php
+++ tests/TiempoRealTest.php
@@ -8,14 +8,12 @@ class TiempoRealTest extends TestCase {
 
     protected $tiempoDePrueba;
     
-    public function __Construct () {
-        $this->tiempoDePrueba = New TiempoReal();
-    }
-    
     /*
      *  Verifica que el tiempo avanza correctamente
      */
     public function testTime () {
+        $this->tiempoDePrueba = New TiempoReal();
+        
         $prediccion = $this->tiempoDePrueba->time();
         sleep(5);
         $this->assertEquals(($this->tiempoDePrueba->time() - 5), $prediccion);

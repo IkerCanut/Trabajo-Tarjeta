2411951c40e5c8eaa9e0342fe888f3f2f5e0cc48
Agustin-Fernandez <agustin.fernandez.tdlzal@gmail.com>
2019-11-12 09:37:22 -0300

    Prototipo de tests de Colectivo

--- tests/ColectivoTest.php
+++ tests/ColectivoTest.php
@@ -5,39 +5,21 @@ namespace TrabajoTarjeta;
 use PHPUnit\Framework\TestCase;
 
 class ColectivoTest extends TestCase {
+    
+    public function testLinea () {
+        $this->assertTrue(TRUE);
+    }
+    
+    public function testEmpresa () {
+        $this->assertTrue(TRUE);
+    }
+    
+    public function testNumero () {
+        $this->assertTrue(TRUE);
+    }
+    
+    public function testPagarCon () {
+        $this->assertTrue(TRUE);
+    }
 
-    /*public function testPagarConSaldo() {
-        $time = new TiempoFalso(0);
-        $bondi = new Colectivo("K", "Empresa generica", 3, $time);
-        $tarjeta = new Tarjeta($time);
-        $tarjeta->recargar(100);
-        $this->assertEquals($tarjeta->obtenerPlus(), 0);
-    }
-    
-    public function testInfoColectivo(){
-        $colectivo = new Colectivo("K", "Empresa generica", 3, new TiempoFalso(0));
-        $this->assertEquals($colectivo->linea(), "K");
-        $this->assertEquals($colectivo->empresa(), "Empresa generica");
-        $this->assertEquals($colectivo->numero(), 3);
-    }
-    
-    public function testPagarSinSaldo() {
-        $tiempo = new TiempoFalso(0);
-        $bondi = new Colectivo("K", "Empresa generica", 3, $tiempo);
-        $tarjeta = new Tarjeta($tiempo);
-        $tarjeta->recargar(20);
-        $valordebido = 20 - $tarjeta->precio;
-        $this->assertEquals($tarjeta->obtenerSaldo(), $valordebido); // WTF DUDE
-        $this->assertEquals($tarjeta->obtenerPlus(), 0);
-        $tiempo->avanzar(6000);
-        $this->assertEquals($tarjeta->obtenerSaldo(), $valordebido);
-        $this->assertEquals($tarjeta->obtenerPlus(), 1);
-        $tiempo->avanzar(6000);
-        $this->assertEquals($tarjeta->obtenerSaldo(), $valordebido);
-        $this->assertEquals($tarjeta->obtenerPlus(), 2);
-        $tiempo->avanzar(6000);
-        $this->assertEquals($bondi->pagarCon($tarjeta), false);
-        $this->assertEquals($tarjeta->obtenerSaldo(), $valordebido);
-        $this->assertEquals($tarjeta->obtenerPlus(), 2);
-    }*/
 }
---
+++ tests/TiempoFalsoTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace TrabajoTarjeta;
+
+use PHPUnit\Framework\TestCase;
+
+class TiempoFalsoTest extends TestCase {
+
+    protected $tiempoDePrueba;
+    
+    
+}

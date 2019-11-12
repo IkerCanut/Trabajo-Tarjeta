d7cd8ce4477b79fbf7ca729469f3e81774c31246
Agustin-Fernandez <agustin.fernandez.tdlzal@gmail.com>
2019-11-12 09:55:50 -0300

    Prototipo de tests de TiempoFalso

--- tests/TiempoFalsoTest.php
+++ tests/TiempoFalsoTest.php
@@ -8,5 +8,13 @@ class TiempoFalsoTest extends TestCase {
 
     protected $tiempoDePrueba;
     
+    public function testAvanzar () {
+        
+        $this->assertTrue(TRUE);
+    }
     
+    public function testTime () {
+        
+        $this->assertTrue(TRUE);
+    }
 }

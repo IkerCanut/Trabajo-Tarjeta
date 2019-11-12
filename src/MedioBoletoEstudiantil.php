eaa6342aef224e6925ed06abce084ad66e0d8187
Agustin-Fernandez <agustin.fernandez.tdlzal@gmail.com>
2019-11-12 17:26:42 -0300

    Intento de corregir otro error de testCheckSaldo en LogicaDePagoTest

--- src/MedioBoletoEstudiantil.php
+++ src/MedioBoletoEstudiantil.php
@@ -5,7 +5,8 @@ namespace TrabajoTarjeta;
 class MedioBoletoEstudiantil extends Tarjeta {
     protected $constantes;
 
-    public function __construct() {
+    public function __construct($saldo = 0) {
+        $this->saldo = $saldo;
         $this->constantes = new Constantes();
         $this->precio = $this->constantes->precioMedioBoletoUniversitario;
         $this->valoresCargables = $this->constantes->cargasPosibles;

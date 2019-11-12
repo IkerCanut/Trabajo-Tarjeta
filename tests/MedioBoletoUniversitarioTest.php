3c063edd5c64f38b2b2a97093102d11d5fe4063c
Agustin-Fernandez <agustin.fernandez.tdlzal@gmail.com>
2019-11-12 15:02:42 -0300

    Agregado testObtenerPrecio en MedioBoletoUniversitarioTest

--- tests/MedioBoletoUniversitarioTest.php
+++ tests/MedioBoletoUniversitarioTest.php
@@ -6,16 +6,33 @@ use PHPUnit\Framework\TestCase;
 
 class MedioBoletoUniversitarioTest extends TestCase {
 
+    protected $tarjetaDePrueba;
+    
     /*
      *  Verifica que se construya correctemante la instancia.
      */
     public function testConstruct () {
         
-        $this->assertTrue(TRUE);
+        $this->tarjetaDePrueba = New MedioBoletoUniversitario();
+        $this->constantesDePrueba = New Constantes();
+        
+        $this->assertEquals($this->tarjetaDePrueba->obtenerPrecio(),$this->constantesDePrueba->precioMedioBoletoUniversitario);
     }
     
     public function testObtenerPrecio () {
         
-        $this->assertTrue(TRUE);
+        $this->tarjetaDePrueba = New MedioBoletoUniversitario();
+        $this->constantesDePrueba = New Constantes();
+        
+        $i = 0;
+        
+        
+        for($i = 0; $i < $this->constantesDePrueba->viajesUniversitariosPorDia; $i++) {
+            
+            $this->assertEquals($this->tarjetaDePrueba->obtenerPrecio(),$this->constantesDePrueba->$this->constantes->precioMedioBoletoUniversitario);
+            $this->tarjetaDePrueba->viajesDiarios ++;
+        }
+        
+        $this->assertEquals($this->tarjetaDePrueba->obtenerPrecio(),$this->constantesDePrueba->$this->constantes->precioBoletoUniversitario);
     }
 }
